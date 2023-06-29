74fc92648ac0ed2b9ea9c5f358c60e61b1863418
MaddyCole13 <colemadalynn1@gmail.com>
2023-06-29 17:58:50 -0600

    first commit

---
+++ src/main/java/com/promineotech/jeep/JeepSales.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.promineotech.jeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+/**
+ * @author maddycole
+ *
+ */
+public class JeepSales {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(JeepSales.class, args); 
+
+	}
+
+}
